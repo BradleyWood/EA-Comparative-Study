e5df347049163fa391f3bd031a9a612b100b626f
Brad <bradley.wood@uoit.net>
2017-10-26 16:09:34 -0400

    Rosenbrock's function

---
+++ src/main/java/algorithms/project/benchmark/Rosenbrock.java
@@ -0,0 +1,14 @@
+package algorithms.project.benchmark;
+
+public class Rosenbrock implements Benchmark {
+
+    @Override
+    public float benchmark(Float... params) {
+        float fitness = 0;
+
+        for (int i = 0; i < params.length - 1; i++) {
+            fitness += 100 * (Math.pow(params[i] * params[i] - params[i + 1], 2) + Math.pow(params[i] - 1, 2));
+        }
+        return fitness;
+    }
+}
