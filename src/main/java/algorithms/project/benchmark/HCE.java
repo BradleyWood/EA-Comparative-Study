f1d40da6e096395e67822a2ca70050aca0ab3739
Brad <bradley.wood@uoit.net>
2017-10-26 15:13:35 -0400

    High conditioned elliptic function

---
+++ src/main/java/algorithms/project/benchmark/HCE.java
@@ -0,0 +1,17 @@
+package algorithms.project.benchmark;
+
+/**
+ * High conditioned elliptic function
+ */
+public class HCE implements Benchmark {
+
+    @Override
+    public float benchmark(Float... params) {
+        float fitness = 0;
+
+        for (int i = 0; i < params.length; i++) {
+            fitness += Math.pow(1000000, (i - 1) / (params.length - 1)) * params[i] * params[i];
+        }
+        return fitness;
+    }
+}
