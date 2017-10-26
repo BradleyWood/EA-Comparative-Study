3cad825be37362ff76460ad0f333c60804c9fd64
Brad <bradley.wood@uoit.net>
2017-10-26 15:24:13 -0400

    Bent Cigar

---
+++ src/main/java/algorithms/project/benchmark/BentCigar.java
@@ -0,0 +1,15 @@
+package algorithms.project.benchmark;
+
+public class BentCigar implements Benchmark {
+
+    @Override
+    public float benchmark(Float... params) {
+        float sq0 = params[0] * params[0];
+
+        float sumOfSquares1ToN = 0;
+        for (int i = 1; i < params.length; i++) {
+            sumOfSquares1ToN += params[i] * params[i];
+        }
+        return sq0 + 1000000 * sumOfSquares1ToN;
+    }
+}
